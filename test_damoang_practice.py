e2d42443bce90b9a5c62f734da7229ddec276d45
minsup <minsup@gmail.com>
2024-06-30 19:48:12 -0700

    Add more comments and remove unnecessary lines.

--- test_damoang_practice.py
+++ test_damoang_practice.py
@@ -13,34 +13,32 @@ def test_free_board_read():
     driver.implicitly_wait(5)
 
     title = driver.title
-    print(title)
     assert title.startswith('자유게시판')
 
     list_element = driver.find_element(By.CSS_SELECTOR, ".da-link-block.subject-ellipsis")
     href = list_element.get_attribute("href")
+    assert href.startswith("https://damoang.net/free/")  # 목록 item 링크 주소 확인
     list_title = list_element.get_attribute("innerText")
-    assert href.startswith("https://damoang.net/free/")
 
-    ActionChains(driver).click(list_element).perform()
+    ActionChains(driver).click(list_element).perform()  # 목록 item 클릭
     sleep(1)
-
-    assert driver.current_url == href
+    assert driver.current_url == href  # 이동한 page의 url과 전 링크주소 매칭 확인
     
     article_element = driver.find_element(By.ID, "bo_v")
     title = article_element.find_element(By.ID, "bo_v_title")
-    assert title.get_attribute("innerText") == list_title
+    assert title.get_attribute("innerText") == list_title  # 이전 목록 text와 article title 일치하는가?
     try:
-        info_panel = article_element.find_element(By.ID, "bo_v_info")
+        info_panel = article_element.find_element(By.ID, "bo_v_info")  # Info Panel 존재 여부
     except:
         assert False, f"Info panel (bo_v_info) not found"
     
     try:
-        article_body = article_element.find_element(By.ID, "bo_v_atc")
+        article_body = article_element.find_element(By.ID, "bo_v_atc")  # 글 body 존재 여부
     except:
         assert False, f"Article body (bo_v_atc) not found"
         
     try:
-        comment_section = article_element.find_element(By.ID, "viewcomment")
+        comment_section = article_element.find_element(By.ID, "viewcomment")  # 답글 창 존재 여부
     except:
         assert False, f"Comment section (viewcomment) not found"
 
